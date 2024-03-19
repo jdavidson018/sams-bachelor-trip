f4aeb3dbce6fc8a153ce82e50db7a6664c4313c6
Jack Davidson <jdavidson018@gmail.com>
2024-03-18 19:57:59 -0600

    feat: Added all pages required for basic functionality

--- src/config/docs.ts
+++ src/config/docs.ts
@@ -20,203 +20,207 @@ export const docsConfig: DocsConfig = {
           href: "/docs/itinerary/dates/",
         },
         {
-          title: "Airbnb/Hotel Options",
-          href: "/docs/getting-started/",
+          title: "Lodging Options",
+          href: "/docs/itinerary/lodging",
         },
         {
           title: "Bars",
-          href: "/docs/getting-started/",
+          href: "/docs/itinerary/bars",
         },
         {
           title: "Restaurants",
-          href: "/docs/getting-started/",
+          href: "/docs/itinerary/restaurants",
         },
         {
           title: "Activities",
-          href: "/docs/getting-started/",
+          href: "/docs/itinerary/activities",
+        },
+        {
+          title: "Full Timeline",
+          href: "/docs/itinerary/timeline",
         },
       ],
     },
-    {
-      title: "Bios",
-      items: [
-        {
-          title: "Introduction",
-          href: "/docs/bios/introduction",
-        },
-        {
-          title: "The Man Himself",
-          href: "/docs/bios/the-man-himself",
-        },
-        {
-          title: "Aaron Cronican",
-          href: "/docs/bios/cronican",
-        },
-        {
-          title: "Adam DeVries",
-          href: "/docs/bios/devries",
-        },
-        {
-          title: "Brendan Obarr",
-          href: "/docs/bios/obarr",
-        },
-        {
-          title: "Carson Post",
-          href: "/docs/bios/post",
-        },
-        {
-          title: "Chris McCann",
-          href: "/docs/bios/mccann",
-        },
-        {
-          title: "Darren Ashton",
-          href: "/docs/bios/ashton",
-        },
-        {
-          title: "Drew Ormseth",
-          href: "/docs/bios/ormseth",
-        },
-        {
-          title: "Grant Starkey",
-          href: "/docs/bios/gstarkey",
-        },
-        {
-          title: "Jack Davidson",
-          href: "/docs/bios/jdavidson",
-        },
-        {
-          title: "Jack Gallagher",
-          href: "/docs/bios/gallagher",
-        },
-        {
-          title: "Jaren Thompson",
-          href: "/docs/bios/thompson",
-        },
-        {
-          title: "Kendall Post",
-          href: "/docs/bios/post",
-        },
-        {
-          title: "Landon Arnold",
-          href: "/docs/bios/arnold",
-        },
-        {
-          title: "Logan Reitz",
-          href: "/docs/bios/reitz",
-        },
-        {
-          title: "Luke Davidson",
-          href: "/docs/bios/ldavidson",
-        },
-        {
-          title: "Max Solow",
-          href: "/docs/bios/solow",
-        },
-        {
-          title: "Mike Davidson",
-          href: "/docs/bios/mdavidson",
-        },
-        {
-          title: "Nick Lambert",
-          href: "/docs/bios/lambert",
-        },
-        {
-          title: "Patrick Albright",
-          href: "/docs/bios/albright",
-        },
-        {
-          title: "Ryan Baber",
-          href: "/docs/bios/baber",
-        },
-        {
-          title: "Safwan Elkhatib",
-          href: "/docs/bios/elkhatib",
-        },
-        {
-          title: "Wade Starkey",
-          href: "/docs/bios/wstarkey",
-        }
-      ],
-    },
-    {
-      title: "Blog",
-      items: [
-        {
-          title: "Introduction",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Build your own",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Writing Posts",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-      ],
-    },
-    {
-      title: "Dashboard",
-      items: [
-        {
-          title: "Introduction",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Layouts",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Server Components",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Authentication",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Database with Prisma",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "API Routes",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-      ],
-    },
-    {
-      title: "Marketing Site",
-      items: [
-        {
-          title: "Introduction",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "File Structure",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Tailwind CSS",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-        {
-          title: "Typography",
-          href: "/docs/in-progress",
-          disabled: true,
-        },
-      ],
-    },
+    // {
+    //   title: "Bios",
+    //   items: [
+    //     {
+    //       title: "Introduction",
+    //       href: "/docs/bios/introduction",
+    //     },
+    //     {
+    //       title: "The Man Himself",
+    //       href: "/docs/bios/the-man-himself",
+    //     },
+    //     {
+    //       title: "Aaron Cronican",
+    //       href: "/docs/bios/cronican",
+    //     },
+    //     {
+    //       title: "Adam DeVries",
+    //       href: "/docs/bios/devries",
+    //     },
+    //     {
+    //       title: "Brendan Obarr",
+    //       href: "/docs/bios/obarr",
+    //     },
+    //     {
+    //       title: "Carson Post",
+    //       href: "/docs/bios/cpost",
+    //     },
+    //     {
+    //       title: "Chris McCann",
+    //       href: "/docs/bios/mccann",
+    //     },
+    //     {
+    //       title: "Darren Ashton",
+    //       href: "/docs/bios/ashton",
+    //     },
+    //     {
+    //       title: "Drew Ormseth",
+    //       href: "/docs/bios/ormseth",
+    //     },
+    //     {
+    //       title: "Grant Starkey",
+    //       href: "/docs/bios/gstarkey",
+    //     },
+    //     {
+    //       title: "Jack Davidson",
+    //       href: "/docs/bios/jdavidson",
+    //     },
+    //     {
+    //       title: "Jack Gallagher",
+    //       href: "/docs/bios/gallagher",
+    //     },
+    //     {
+    //       title: "Jaren Thompson",
+    //       href: "/docs/bios/thompson",
+    //     },
+    //     {
+    //       title: "Kendall Post",
+    //       href: "/docs/bios/kpost",
+    //     },
+    //     {
+    //       title: "Landon Arnold",
+    //       href: "/docs/bios/arnold",
+    //     },
+    //     {
+    //       title: "Logan Reitz",
+    //       href: "/docs/bios/reitz",
+    //     },
+    //     {
+    //       title: "Luke Davidson",
+    //       href: "/docs/bios/ldavidson",
+    //     },
+    //     {
+    //       title: "Max Solow",
+    //       href: "/docs/bios/solow",
+    //     },
+    //     {
+    //       title: "Mike Davidson",
+    //       href: "/docs/bios/mdavidson",
+    //     },
+    //     {
+    //       title: "Nick Lambert",
+    //       href: "/docs/bios/lambert",
+    //     },
+    //     {
+    //       title: "Patrick Albright",
+    //       href: "/docs/bios/albright",
+    //     },
+    //     {
+    //       title: "Ryan Baber",
+    //       href: "/docs/bios/baber",
+    //     },
+    //     {
+    //       title: "Safwan Elkhatib",
+    //       href: "/docs/bios/elkhatib",
+    //     },
+    //     {
+    //       title: "Wade Starkey",
+    //       href: "/docs/bios/wstarkey",
+    //     }
+    //   ],
+    // },
+    // {
+    //   title: "Blog",
+    //   items: [
+    //     {
+    //       title: "Introduction",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "Build your own",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "Writing Posts",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: "Dashboard",
+    //   items: [
+    //     {
+    //       title: "Introduction",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "Layouts",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "Server Components",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "Authentication",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "Database with Prisma",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "API Routes",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: "Marketing Site",
+    //   items: [
+    //     {
+    //       title: "Introduction",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "File Structure",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "Tailwind CSS",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //     {
+    //       title: "Typography",
+    //       href: "/docs/in-progress",
+    //       disabled: true,
+    //     },
+    //   ],
+    // },
   ],
 }
